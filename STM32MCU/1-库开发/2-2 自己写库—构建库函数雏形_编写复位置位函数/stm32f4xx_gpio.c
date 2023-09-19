713eb1c1bd27f801f4a1b74fdb7d13d292c08b54
Ethan Wang <44759507+artwalker@users.noreply.github.com>
2023-09-19 18:04:25 +0800

    Update stm32f4xx_gpio.c

--- STM32MCU/1-库开发/2-2 自己写库—构建库函数雏形_编写复位置位函数/stm32f4xx_gpio.c
+++ STM32MCU/1-库开发/2-2 自己写库—构建库函数雏形_编写复位置位函数/stm32f4xx_gpio.c
@@ -2,11 +2,13 @@
 
 void GPIO_SetBits(GPIO_TypeDef *GPIOx,uint16_t GPIO_Pin)
 {
+	//Reset重置给1后，IO端口输出0
 	GPIOx->BSRRL = GPIO_Pin;
 }
 
 void GPIO_ResetBits(GPIO_TypeDef *GPIOx,uint16_t GPIO_Pin)
 {
+	//set重置给1后，IO端口输出1
 	GPIOx->BSRRH = GPIO_Pin;
 }
 
